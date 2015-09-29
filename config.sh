ac7029b819818217131d364f7c42a7a7971db7d0
Peter Halliday <pghalliday@gmail.com>
2015-09-29 19:04:27 +0200

    add default bash stuff from arch linux

--- config.sh
+++ config.sh
@@ -1,3 +1,11 @@
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+
+# add local gem binaries to path
+export PATH=$PATH:~/.gem/ruby/2.2.0/bin
 
 for file in ~/.bash/config/*
   do . $file
