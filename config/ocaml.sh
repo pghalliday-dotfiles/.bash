98e24fb7bbedd2cd1f254e8aa2828ccdb228a74e
Peter Halliday <pghalliday@gmail.com>
2017-05-25 20:58:21 +0200

    ocaml env

---
+++ config/ocaml.sh
@@ -0,0 +1,6 @@
+CAML_LD_LIBRARY_PATH="$HOME/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"; export CAML_LD_LIBRARY_PATH;
+OPAMUTF8MSGS="1"; export OPAMUTF8MSGS;
+MANPATH=":$HOME/.opam/system/man"; export MANPATH;
+PERL5LIB="$HOME/.opam/system/lib/perl5"; export PERL5LIB;
+OCAML_TOPLEVEL_PATH="$HOME/.opam/system/lib/toplevel"; export OCAML_TOPLEVEL_PATH;
+PATH=$HOME/.opam/system/bin:$PATH; export PATH;
